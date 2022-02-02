125fde776526bd6f0bc25886ccb6f182d36628d5
StefanKostenko <90892654+StefanKostenko@users.noreply.github.com>
2022-02-02 13:20:28 +0100

    Update HolaMundo.java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,5 +2,8 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
         System.out.println("Primera Refactorizacion!");
+        
+        System.out.println("Cambio en GitHub para un pull!");
+
     }
 }
